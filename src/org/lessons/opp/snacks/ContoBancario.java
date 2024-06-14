113391466b2c3bd1c9c5d3efd05b63864c3b12ba
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-14 16:10:03 +0200

    add instance variables and constructor method to ContoBancario.java

--- src/org/lessons/opp/snacks/ContoBancario.java
+++ src/org/lessons/opp/snacks/ContoBancario.java
@@ -2,5 +2,13 @@ package org.lessons.opp.snacks;
 
 public class ContoBancario {
 	
+	// instance variables
+	private int accountNumber;
+	private double accountBalance;
 	
+	// constructor method
+	public ContoBancario(int accountNumber) {
+		this.accountNumber = accountNumber;
+		this.accountBalance = 0.0d;
+	}
 }
