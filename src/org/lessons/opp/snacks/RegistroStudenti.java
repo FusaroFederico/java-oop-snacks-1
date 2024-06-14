49e6a51a2a8889bbf31de133f2ef852dd0bdd1e6
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-14 17:00:19 +0200

    add instance array and constructor method to RegistroStudenti.java

--- src/org/lessons/opp/snacks/RegistroStudenti.java
+++ src/org/lessons/opp/snacks/RegistroStudenti.java
@@ -1,5 +1,11 @@
 package org.lessons.opp.snacks;
 
 public class RegistroStudenti {
-
+	// instance array of students objects
+	private Studente[] studentRegister; 
+	
+	// constructor method
+	public RegistroStudenti() {
+		studentRegister = new Studente[30]; // max 30 students
+	}
 }
