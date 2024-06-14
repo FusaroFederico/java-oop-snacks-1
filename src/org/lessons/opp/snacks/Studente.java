11b8796e7758df6fcb9a17c8e17bbd10b40f3b92
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-14 15:56:35 +0200

    add instance variables to Studente.java

--- src/org/lessons/opp/snacks/Studente.java
+++ src/org/lessons/opp/snacks/Studente.java
@@ -1,6 +1,11 @@
 package org.lessons.opp.snacks;
 
 public class Studente {
+	// instance variables
+	private String firstName;
+	private String lastName;
+	private int age;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
