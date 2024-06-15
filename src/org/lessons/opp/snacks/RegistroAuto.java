fb7f318da9625e6363638a703e7859c50a28e093
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-15 09:15:11 +0200

    add constructor method to RegistroAuto.java

--- src/org/lessons/opp/snacks/RegistroAuto.java
+++ src/org/lessons/opp/snacks/RegistroAuto.java
@@ -1,5 +1,10 @@
 package org.lessons.opp.snacks;
 
 public class RegistroAuto {
-
+	// instance array of Auto objects
+	private Auto[] carRegister;
+	// constructor method
+	public RegistroAuto() {
+		carRegister = new Auto[100];
+	}
 }
