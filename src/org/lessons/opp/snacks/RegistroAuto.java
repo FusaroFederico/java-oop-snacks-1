8c3a34f6911f12e7937298c3eff7ea097b46cdc0
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-15 09:19:12 +0200

    add addCar method to RegistroAuto.java

--- src/org/lessons/opp/snacks/RegistroAuto.java
+++ src/org/lessons/opp/snacks/RegistroAuto.java
@@ -3,8 +3,14 @@ package org.lessons.opp.snacks;
 public class RegistroAuto {
 	// instance array of Auto objects
 	private Auto[] carRegister;
+	private int counter = 0;
 	// constructor method
 	public RegistroAuto() {
 		carRegister = new Auto[100];
+	}
+	// addCar method
+	public void addCar(Auto car) {
+		carRegister[counter] = car;
+		counter ++;
 	}
 }
