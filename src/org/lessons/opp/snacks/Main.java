42dfa51bfd9c31cf523a7aa42dad3e53785e7a90
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-27 09:32:13 +0200

    Update Main.java

--- src/org/lessons/opp/snacks/Main.java
+++ src/org/lessons/opp/snacks/Main.java
@@ -1,12 +1,23 @@
 package org.lessons.opp.snacks;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String txtPath = "C:\\Users\\fusar\\Documents\\Boolean\\esercizi-backend\\java-oop-snacks-1\\redme.txt";
-		System.out.println(TextReader.readFirstLine(txtPath));
+		//String txtPath = "C:\\Users\\fusar\\Documents\\Boolean\\esercizi-backend\\java-oop-snacks-1\\readme.txt";
+		//System.out.println(TextReader.readFirstLine(txtPath));
+		
+		ArrayList<String> nomi = new ArrayList<String>();
+		nomi.add("Riccardo");
+		nomi.add("Giuseppe");
+		nomi.add("Francesco");
+		nomi.add("Anna");
+		nomi.add("Flavia");
+		
+		String nome = "Riccardo";
+		System.out.println(ArrayListUtils.getIndexOf(nomi, nome));
 	}
 
 }
