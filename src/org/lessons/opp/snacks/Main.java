a022c0b847b5a3daddb9bbaf2ee9711145d2e5b7
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-27 10:01:12 +0200

    Update Main.java

--- src/org/lessons/opp/snacks/Main.java
+++ src/org/lessons/opp/snacks/Main.java
@@ -2,6 +2,7 @@ package org.lessons.opp.snacks;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
 
@@ -9,15 +10,29 @@ public class Main {
 		//String txtPath = "C:\\Users\\fusar\\Documents\\Boolean\\esercizi-backend\\java-oop-snacks-1\\readme.txt";
 		//System.out.println(TextReader.readFirstLine(txtPath));
 		
-		ArrayList<String> nomi = new ArrayList<String>();
-		nomi.add("Riccardo");
-		nomi.add("Giuseppe");
-		nomi.add("Francesco");
-		nomi.add("Anna");
-		nomi.add("Flavia");
+		//ArrayList<String> nomi = new ArrayList<String>();
+		//nomi.add("Riccardo");
+		//nomi.add("Giuseppe");
+		//nomi.add("Francesco");
+		//nomi.add("Anna");
+		//nomi.add("Flavia");
 		
-		String nome = "Riccardo";
-		System.out.println(ArrayListUtils.getIndexOf(nomi, nome));
+		//String nome = "Riccardo";
+		//System.out.println(ArrayListUtils.getIndexOf(nomi, nome));
+		
+		HashSet<String> set1 = new HashSet<String>();
+        set1.add("mela");
+        set1.add("banana");
+        set1.add("pesca");
+        set1.add("melone");
+
+        HashSet<String> set2 = new HashSet<String>();
+        set2.add("melone");
+        set2.add("mela");
+        set2.add("pesca");
+        set2.add("banana");
+        
+        System.out.println(HashSetUtils.compareTo(set1, set2));
 	}
 
 }
